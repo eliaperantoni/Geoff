01f835dc81591176eadcfb6ef22e83d77f516e5f
Nicolò Fretti <nico.fretti@gmail.com>
2020-06-02 15:39:00 +0200

    [Start]Login empty

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import * as firebase from "firebase/app";
+
+//---LOGIN
+
+//ASSINCRONO
+//const i = firebase.auth().signInWithEmailAndPassword(em,pw);
+//i.then((params)=>{console.log(params)});
+//---OPPURE
+//const i = await firebase.auth().signInWithEmailAndPassword(em,pw);
+//QUI I CONTIENE IL VALORE EFFETTIVO, ASPETTO IL VALORE
+
+//CONTEXT ASYNC POSSO SOLO FARLO QUI IL AWAI PER METTERE TUTTE LE COSE ASINCRONE DIVISE SENZA
+//DOVER STOPPARE IL MAIN, POSSO CHIAMARLA OVUNQUE
+//async function f(){
+    //POSSO METTERE
+//}
+//f().then();//DON'T STOP CODE
+//await f();//STOP CODE
 
 const Header = styled.div`
   width:100%;
