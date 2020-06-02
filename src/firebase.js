94c9a85362735f859823264a731a6404f5fd84d9
Elia Perantoni <perantonielia0@gmail.com>
2020-06-02 15:19:35 +0200

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAd8nAsiHadG6b1BTDI6RcT0VIFH7vfnr8",
+    authDomain: "geoff-e72fa.firebaseapp.com",
+    databaseURL: "https://geoff-e72fa.firebaseio.com",
+    projectId: "geoff-e72fa",
+    storageBucket: "geoff-e72fa.appspot.com",
+    messagingSenderId: "893330948376",
+    appId: "1:893330948376:web:24ffce52a03b0bff2b9e6d",
+    measurementId: "G-QNHL0YQ967"
+};
+
+firebase.initializeApp(firebaseConfig);
