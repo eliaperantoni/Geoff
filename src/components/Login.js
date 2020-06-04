6ce8caca63b72947b15974f999ff20bbd3f69de4
Nicolò Fretti <nico.fretti@gmail.com>
2020-06-04 21:15:42 +0200

    [Add]Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Card from "./basic/Card"
 import Button from "./basic/Button";
 import Input from "./basic/Input";
+import image from "../img/authentication.svg"
 import * as firebase from "firebase/app";
 
 //---LOGIN
@@ -22,7 +23,7 @@ import * as firebase from "firebase/app";
 //f().then();//DON'T STOP CODE
 //await f();//STOP CODE
 
-const Container = styled.div`
+const Container = styled(Card)`
     min-width: 400px;
     min-height: 500px; 
     flex: 1;
@@ -31,13 +32,15 @@ const Container = styled.div`
     justify-content: center;
     align-items: center;
     background:#E5E5E5;
-`;
-const Form = styled.div`
+    overflow: hidden;
+`
+const Form = styled(Card)`
     display:flex;
     flex-direction: column;
+    align-items: center;
     text-align:center;
     min-width: 400px;
-    min-height: 500px; 
+    min-height: 550px; 
     background: #FAFDFF;
     border-radius: 24px;
     box-shadow: 0 2px 64px rgba(232,238,243,0.5);
@@ -48,7 +51,8 @@ const Upper = styled.div`
   flex-direction: column;
   justify-content: space-between;
   margin-bottom: auto;
-  flex-grow: 0.09;
+  flex-grow: 0.2;
+  margin-top: 100px;
 `
 const Bottom = styled.div`
   display: flex;
@@ -56,14 +60,23 @@ const Bottom = styled.div`
   justify-content: space-between;
   margin-top: auto;
   flex-grow: 0.09;
-  
 `
-
+const Immage = styled.img`
+    max-width: 340px;
+    max-height: 340px;
+    margin-top: -140px;
+ `
+const Par = styled.p`
+    margin-top:20px;
+    font-family: FuturaLight, sans-serif;
+    font-size: 22px;
+ `
 export default function Login(props) {
     return (
+
         <Container>
             <Form>
-                <p src="">c</p>
+                <Immage src={image}/>
                 <Upper>
                     <Input placeholder="Email"></Input>
                     <Input placeholder="Password"></Input>
@@ -72,7 +85,7 @@ export default function Login(props) {
                     <Button>Login</Button>
                     <Button>Don't have an account?</Button>
                 </Bottom>
-
+                <Par>Forgot password ?</Par>
             </Form>
         </Container>
     );
--- src/components/basic/Input.js
+++ src/components/basic/Input.js
@@ -6,7 +6,8 @@ const Input = styled.input`
     border-radius: 8px;
     font-family: FuturaLight, sans-serif;
     font-size: 22px;
-    padding: 12px 24px;
+    padding: 12px 64px;
+    //padding: 12px 24px;
     box-shadow: 0 2px 8px rgba(176,195,215,0.26);
     
     &::placeholder {
