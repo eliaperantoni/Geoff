1d038c1ea5eeff7c492f6eba53c5534826233f06
Elia Perantoni <perantonielia0@gmail.com>
2020-07-10 18:28:46 +0200

    IconButton fix path fill color

--- src/components/basic/IconButton.js
+++ src/components/basic/IconButton.js
@@ -6,11 +6,17 @@ const StyledIconButton = styled.div`
     background: linear-gradient(-90deg, #00e74e, #6BE595);
     box-shadow: 0 2px 8px rgba(90,198,127,0.81);
     text-shadow: 0 2px 4px rgba(64,187,108,0.56);
+    path {
+        fill: #DFFFEA !important;
+    }
     
     ${props => props.type === "danger" && css`
         background: linear-gradient(-90deg, #D80142, #DD4673);
         box-shadow: 0 2px 8px #AD1341;
         text-shadow: 0 2px 4px #FFD2E0;
+        path {
+            fill: #FFD2E0 !important;
+        }
     `}
     
     border-radius: 8px;
@@ -25,7 +31,7 @@ const StyledIconButton = styled.div`
 export default function IconButton({type, icon}){
     return(
         <StyledIconButton type={type}>
-            <Icon color="#DFFFEA" size={1} path={icon}/>
+            <Icon size={1} path={icon}/>
         </StyledIconButton>
     )
 }
