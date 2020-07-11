678913f7d834e8c37737e74001fab07e1b3d68bf
Nicolò Fretti <nico.fretti@gmail.com>
2020-07-11 15:53:18 +0200

    Basket change quantity

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import styled from "styled-components";
 import Price from "components/basic/Price";
+import firebase from "firebase.js";
 
 const StyledItem = styled.div`
   display: flex;
@@ -16,8 +17,8 @@ const StyledItem = styled.div`
 
 const ItemImage = styled.img`
   border-radius: 10px;
-  height: 50px;
-  width: 50px;
+  height: 48px;
+  width: 48px;
   object-fit: cover;
 `;
 
@@ -31,40 +32,89 @@ const ItemDetails = styled.div`
 
 const ItemName = styled.span`
   font-family: FuturaBold, sans-serif;
-  font-size: 1.0rem;
+  font-size: 0.9rem;
 `;
 
 const ItemUnitPrice = styled(Price)`
   font-family: FuturaLight, sans-serif;
-  font-size: 0.9rem;
+  font-size: 0.7rem;
 `;
 
 const ItemTotalOrder = styled.input`
   font-family: FuturaLight, sans-serif;
   font-size: 1.1rem;
   color: #91A6B8;
-  padding: 10px 20px;
+  padding: 10px 10px;
   border:none;
-  width: 20px;
+  width: 50px;
   border-radius: 8px;
   background-color: #ffffff;
-  -moz-appearance: textfield;
-  -webkit-appearance: none;
-  
   margin: 0;
 `;
 
-function OrderItem({item}) {
-    return (
+
+class OrderItem extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state ={
+            item:props.item,
+            quantity: props.quantity,
+            handler : props.handler,
+        }
+    }
+    async getUserEmail() {
+        const emailPromise = new Promise(resolve => {
+            const unsubscribe = firebase.auth().onAuthStateChanged(async ({email}) => {
+                unsubscribe();
+                resolve(email);
+            });
+        });
+
+        return await emailPromise;
+    }
+    // Returns an array containing all products of the user with the provided email
+    async modifyUserBasket(email,itemID,value) {
+        const query = await firebase.firestore().collection(`users`).doc(email).get();
+
+        if(!query.exists){
+            alert("empty");
+            return null;
+        }else{
+            const basket = query.data().basket;
+            basket.map((obj,index)=>{
+                if(obj.itemID === itemID){
+                    if(value>0){
+                        basket[index].quantity =  parseInt(value, 10);
+                    }else{
+                        basket.splice(index,1);
+                    }
+                }
+            })
+            return basket;
+        }
+    }
+    changeItem= async (value)=> {
+        if(value===""){
+            value=-1;//RIMUOVO L'ELEMENTO DOPO
+        }
+        let email = await this.getUserEmail();
+        let basket = await this.modifyUserBasket(email,this.state.item.itemID,value)
+        await firebase.firestore().collection(`users`).doc(email).update({basket:basket});
+        await this.state.handler();
+    }
+    render(){
+        return (
         <StyledItem>
-            <ItemImage src={item.image}/>
+            <ItemImage src={this.state.item.image}/>
             <ItemDetails>
-                <ItemName>{item.name}</ItemName>
-                <ItemUnitPrice price={item.price}/>
+                <ItemName>{this.state.item.name}</ItemName>
+                <ItemUnitPrice price={this.state.item.price}/>
             </ItemDetails>
-            <ItemTotalOrder value={item.quantity}/>
+            <ItemTotalOrder maxlength="999" type="number" value={this.state.quantity} onChange={e => this.changeItem(e.target.value)}/>
         </StyledItem>
-    );
+        );
+    }
 }
 export default OrderItem;
 
