64e2e0992824f03e550d587f3a33155daa0df788
Elia Perantoni <perantonielia0@gmail.com>
2020-07-13 21:37:28 +0200

    Removed superfluous code

--- src/components/basic/Button.js
+++ src/components/basic/Button.js
@@ -16,20 +16,6 @@ const Button = styled.button`
         cursor: default;
     `}
     
-    ${props => (!props.type || props.type === "primary") && css`
-        background: linear-gradient(-90deg, #00e74e, #6BE595);
-        color: #e0f8e7;
-        box-shadow: 0 2px 8px rgba(90,198,127,0.81);
-        text-shadow: 0 2px 4px rgba(64,187,108,0.56);
-    `}
-    
-    ${props => props.type === "secondary" && css`
-        background: linear-gradient(-90deg, #00AB3A, #4DB671);
-        color: #D1FFE1;
-        box-shadow: 0 2px 8px rgba(35,147,73,0.7);
-        text-shadow: 0 2px 4px rgba(28,100,53,0.7);
-    `}
-    
     ${props => {
         const colors = {
             primary: {
